8047cc8661a4c20001500540972e09b2dcb74699
Mirko Rainer <mirkorainer@outlook.com>
2020-06-22 21:39:22 -0400

    disable prop types for eslint. Add modal for RN

--- src/PlayerCharacterSheet/1_MainPage/CharacterMetadata/CharacterName.tsx
+++ src/PlayerCharacterSheet/1_MainPage/CharacterMetadata/CharacterName.tsx
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { Component, useState } from "react";
+import { View, StyleSheet, Text, TextInputComponent, TextInput, Button, ShadowPropTypesIOS } from "react-native";
+import Modal from "react-native-modal";
 
 interface Props {
     characterName: string;
@@ -7,18 +8,37 @@ interface Props {
 
 interface State {}
 
-export default class CharacterName extends Component<Props, State> {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>
-                    {" "}
-                    Character Name: {this.props.characterName}{" "}
-                </Text>
-            </View>
-        );
-    }
-}
+const CharacterName: React.FC<Props> = (props) => {
+    const [isModalVisible, setModalVisible] = useState(false);
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
+
+    let lastTap = 0;
+    const handleDoubleTap = () => {
+        const now = Date.now();
+        const DOUBLE_TAP_DELAY = 300;
+        if (lastTap && (now - lastTap) < DOUBLE_TAP_DELAY) {
+            setModalVisible(!isModalVisible);
+        } else {
+            lastTap = now;
+        }
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text} onPress={handleDoubleTap} >
+                {" "}
+                Character Name: {props.characterName}{" "}
+            </Text>
+            <Modal isVisible={isModalVisible}>
+                <View style={styles.modal}>
+                    <TextInput></TextInput>
+                    <Button title="Done" onPress={toggleModal} />
+                </View>
+            </Modal>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -29,4 +49,9 @@ const styles = StyleSheet.create({
         alignSelf: "stretch",
     },
     text: {},
+    modal: {
+        flex: 1
+    }
 });
+
+export default CharacterName;
