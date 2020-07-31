c986d199350afca2280444d2d05558aea7305ef8
Mirko Rainer <mirkorainer@outlook.com>
2020-07-31 06:48:05 -0400

    ACView updated. Removed need for ProficiencyView in ACView

--- src/scenes/CharacterSheet/Encounter/Components/ArmorClass/ACView.tsx
+++ src/scenes/CharacterSheet/Encounter/Components/ArmorClass/ACView.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
 import ProficiencyView, { ProficiencyProps } from "../../../../Shared/ProficiencyView";
-import { wornArmorProficiency } from "./ArmorClassHelper";
+import { getWornArmorProficiency } from "./ArmorClassHelper";
 import { CharacterSheetState } from "../../../../../store/Store";
-import { AbilityScore } from "../../../../../PF2eCoreLib/AbilityScores";
+import { AbilityScore, CalculateAbilityScoreModifier } from "../../../../../PF2eCoreLib/AbilityScores";
 import { ArmorProficiencies, WornArmor } from "../../../../../PF2eCoreLib/PlayerCharacter";
 import { connect } from "react-redux";
 import Shield, { ShieldProps } from "./Shield";
+import { GetProficiencyValue } from "../../../../../PF2eCoreLib/Proficiencies";
+import ProficiencyArrayView from "../../../../Shared/ProficiencyArrayView";
 
 
 const ACView: React.FC<Props> = (props) => {
@@ -14,7 +17,7 @@ const ACView: React.FC<Props> = (props) => {
         return {
             title: "AC",
             keyAbility: props.dexterity,
-            proficiency: wornArmorProficiency(props.armorProficiencies, props.wornArmor.Category),
+            proficiency: getWornArmorProficiency(props.armorProficiencies, props.wornArmor.Category),
             level: props.level,
             itemBonus: props.wornArmor.ACBonus,
             is10base: true,
@@ -23,27 +26,44 @@ const ACView: React.FC<Props> = (props) => {
         };
     };
 
+    const modifier = CalculateAbilityScoreModifier(props.dexterity.score);
+    const wornProficiency = getWornArmorProficiency(props.armorProficiencies, props.wornArmor.Category);
+    const total =
+            10 +
+            modifier +
+            props.level +
+            props.wornArmor.ACBonus +
+            GetProficiencyValue(wornProficiency);
+
     return(
-        <View>
-            <ProficiencyView
-                title={"AC"}
-                keyAbility={
-                    acProficiency().keyAbility
-                }
-                proficiency={acProficiency().proficiency}
-                level={props.level}
-                itemBonus={acProficiency().itemBonus}
-                is10base={acProficiency().is10base}
-                isACBase={true}
-                dexCap={acProficiency().dexCap}
-                armorPenalty={acProficiency().armorPenalty}
-            />
+        <View style={styles.container}>
+            <View style={styles.horizontal}>
+                <Text h4>AC</Text>
+                <Text h4>{total}</Text>
+                <Text> = 10 + DEX:{modifier} Cap:
+                    {props.wornArmor.DexCap !== undefined ? props.wornArmor.DexCap : 0}</Text>
+                <Text>Armor: +{props.wornArmor.ACBonus}</Text>
+                <ProficiencyArrayView proficiency={wornProficiency} />
+            </View>
             <Shield
                 shieldProps={props.shield}
             />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    horizontal: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    centered: {
+        alignSelf: "center"
+    }
+});
 
 type Props = LinkDispatchProps & LinkStateProps;
 
@@ -76,10 +96,3 @@ const mapStateToProps = (
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ACView);
-
-
-const styles = StyleSheet.create({
-    centered: {
-        alignSelf: "center"
-    }
-});
--- src/scenes/CharacterSheet/Encounter/Components/ArmorClass/ArmorClassHelper.ts
+++ src/scenes/CharacterSheet/Encounter/Components/ArmorClass/ArmorClassHelper.ts
@@ -2,7 +2,7 @@ import { ArmorProficiencies } from "../../../../../PF2eCoreLib/PlayerCharacter";
 import { ArmorCategory } from "../../../../../PF2eCoreLib/ArmorCategory";
 import { Proficiencies } from "../../../../../PF2eCoreLib/Proficiencies";
 
-export const wornArmorProficiency = (armorProficiencies: ArmorProficiencies, wornArmorCategory: ArmorCategory): Proficiencies => {
+export const getWornArmorProficiency = (armorProficiencies: ArmorProficiencies, wornArmorCategory: ArmorCategory): Proficiencies => {
     switch (wornArmorCategory) {
     case ArmorCategory.Unarmored: {
         return armorProficiencies.unarmored;
