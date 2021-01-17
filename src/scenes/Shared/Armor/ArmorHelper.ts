003273e1996b337684a4adc27e2522188f4188a3
Mirko Rainer <mirkorainer@outlook.com>
2021-01-17 06:37:59 -0500

    Add helper functions for Armor

---
+++ src/scenes/Shared/Armor/ArmorHelper.ts
@@ -0,0 +1,17 @@
+import { ArmorCategory } from "../../../PF2eCoreLib/ArmorCategory";
+import { ArmorGroup } from "../../../PF2eCoreLib/ArmorGroup";
+import { Dictionary } from "../Misc/Dictionary";
+
+export const ArmorCategoryData: Dictionary<keyof ArmorCategory> = {
+    0: "Unarmored",
+    1: "Light",
+    2: "Medium",
+    3: "Heavy",
+};
+
+export const ArmorGroupData: Dictionary<string> = {
+    0: ArmorGroup.Leather,
+    1: ArmorGroup.Composite,
+    2: ArmorGroup.Chain,
+    3: ArmorGroup.Plate,
+};
