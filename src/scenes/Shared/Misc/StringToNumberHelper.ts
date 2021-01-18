ec535cb46426aadb0b0d5cb9ae025d2faf8889aa
Mirko Rainer <mirkorainer@outlook.com>
2021-01-18 06:43:40 -0500

    Additional string->number helper added.

--- src/scenes/Shared/Misc/StringToNumberHelper.ts
+++ src/scenes/Shared/Misc/StringToNumberHelper.ts
@@ -2,3 +2,7 @@ export const isNumbersOnly = (input: string): boolean => {
     const numberReg = new RegExp(/^\d+$/);
     return numberReg.test(input);
 };
+
+export const isNumbersOnlyElseReturn0 = (input: string): number => {
+    return isNumbersOnly(input) ? parseInt(input) : 0;
+};
