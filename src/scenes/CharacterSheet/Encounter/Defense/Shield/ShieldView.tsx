966c64287cc0d780163c24897a5249e4bc1a4008
Mirko Rainer <mirkorainer@outlook.com>
2021-02-02 06:30:58 -0500

    Two shields!

--- src/scenes/CharacterSheet/Encounter/Defense/Shield/ShieldView.tsx
+++ src/scenes/CharacterSheet/Encounter/Defense/Shield/ShieldView.tsx
@@ -30,172 +30,170 @@ import { iBonus } from "../../../../../PF2eCoreLib/Bonus";
 // TODO: Raise Shield button
 // TODO: Multiple Shields
 const ShieldView: React.FC<Props> = (props) => {
-    if (props.shield === undefined) {
+    if (props.wornShields === undefined) {
         return <></>;
     }
-    const shield = props.shield;
-    // TODO: Long press to EditShield
-    const editShield = () => {};
-
-    const adjustShieldHP = (amount: number) => {
-        const adjustedHP = shield.currentHP + amount;
-        if (adjustedHP <= 0) {
-            props.updateShield({ ...shield, currentHP: 0 });
-            return;
-        }
-        if (adjustedHP > shield.maxHP) {
-            props.updateShield({
-                ...shield,
-                currentHP: shield.maxHP,
-            });
-            return;
-        }
-        props.updateShield({ ...shield, currentHP: adjustedHP });
-    };
-
-    const PlusHPButton = (amount: number) => (
-        //@ts-ignore
-        <Button
-            onPress={() => {
-                adjustShieldHP(amount);
-            }}
-            style={styles.plus}
-        >
-            +{amount.toString()}
-        </Button>
-    );
-    const MinusHPButton = (amount: number) => (
-        <Button
-            onPress={() => {
-                adjustShieldHP(amount);
-            }}
-            style={styles.minus}
-        >
-            {amount.toString()}
-        </Button>
-    );
-
-    const shieldData = () => {
-        if (shield.currentHP > shield.breakThreshold) {
-            return (
-                <>
-                    <Layout style={styles.column}>
-                        <Text style={styles.text}>Bonus:</Text>
-                        <Text style={styles.number}>
-                            +{shield.acBonus.amount}
-                        </Text>
-                    </Layout>
-                    <Layout style={styles.column}>
-                        <Text style={styles.text}>BT:</Text>
-                        <Text style={styles.number}>
-                            {shield.breakThreshold!}
-                        </Text>
-                    </Layout>
-                    <Layout style={styles.column}>
-                        <Text style={styles.text}>Hardness:</Text>
-                        <Text style={styles.number}> {shield.hardness!}</Text>
-                    </Layout>
-                </>
-            );
-        } else {
-            return (
-                <>
-                    <Text
-                        category="h6"
-                        status="warning"
-                        style={{
-                            flex: 1,
-                            alignSelf: "stretch",
-                            textAlign: "center",
-                        }}
-                    >
-                        Broken
-                    </Text>
-                </>
-            );
-        }
-    };
-
-    const ShieldRaisedIcon = (props: any) => (
-        <Icon
-            {...props}
-            style={[props.style, { width: 32, height: 32 }]}
-            name="shield"
-        />
-    );
-    const ShieldDownIcon = (props: any) => (
-        <Icon
-            {...props}
-            style={[props.style, { width: 32, height: 32 }]}
-            name="shield-off"
-        />
-    );
-    const ShieldIcon = shield.isRaised ? ShieldRaisedIcon : ShieldDownIcon;
-    const handleShieldPress = () => {
-        props.raiseShield(shield.acBonus, shield.isRaised);
-        props.updateShield({ ...shield, isRaised: !shield.isRaised });
-    };
-
-    const shieldView = (
-        <Layout style={styles.container}>
-            <Text category="h5" style={{ textAlign: "center" }}>
-                Shield
-            </Text>
-            <Layout style={{ flexDirection: "row" }}>
-                {
-                    // TODO: Finish Raise shield Logic}
-                }
-                {shield.currentHP > shield.breakThreshold ? (
-                    <Button
-                        accessoryRight={ShieldIcon}
-                        appearance="ghost"
-                        onPress={handleShieldPress}
-                    />
-                ) : (
-                    <></>
-                )}
-                <Layout style={styles.shield}>
-                    <Layout style={styles.horizontal}>
-                        <ButtonGroup
-                            style={styles.buttonGroup}
-                            size="tiny"
-                            status="danger"
+    let shields: JSX.Element[] = [];
+    props.wornShields.forEach((shield) => {
+        // TODO: Long press to EditShield
+        const editShield = () => {};
+        const adjustShieldHP = (amount: number) => {
+            const adjustedHP = shield.currentHP + amount;
+            if (adjustedHP <= 0) {
+                props.updateShield({ ...shield, currentHP: 0 });
+                return;
+            }
+            if (adjustedHP > shield.maxHP) {
+                props.updateShield({
+                    ...shield,
+                    currentHP: shield.maxHP,
+                });
+                return;
+            }
+            props.updateShield({ ...shield, currentHP: adjustedHP });
+        };
+        const PlusHPButton = (amount: number) => (
+            //@ts-ignore
+            <Button
+                onPress={() => {
+                    adjustShieldHP(amount);
+                }}
+                style={styles.plus}
+            >
+                +{amount.toString()}
+            </Button>
+        );
+        const MinusHPButton = (amount: number) => (
+            <Button
+                onPress={() => {
+                    adjustShieldHP(amount);
+                }}
+                style={styles.minus}
+            >
+                {amount.toString()}
+            </Button>
+        );
+        const shieldData = () => {
+            if (shield.currentHP > shield.breakThreshold) {
+                return (
+                    <>
+                        <Layout style={styles.column}>
+                            <Text style={styles.text}>Bonus:</Text>
+                            <Text style={styles.number}>
+                                +{shield.acBonus.amount}
+                            </Text>
+                        </Layout>
+                        <Layout style={styles.column}>
+                            <Text style={styles.text}>BT:</Text>
+                            <Text style={styles.number}>
+                                {shield.breakThreshold!}
+                            </Text>
+                        </Layout>
+                        <Layout style={styles.column}>
+                            <Text style={styles.text}>Hardness:</Text>
+                            <Text style={styles.number}>
+                                {" "}
+                                {shield.hardness!}
+                            </Text>
+                        </Layout>
+                    </>
+                );
+            } else {
+                return (
+                    <>
+                        <Text
+                            category="h6"
+                            status="warning"
+                            style={{
+                                flex: 1,
+                                alignSelf: "stretch",
+                                textAlign: "center",
+                            }}
                         >
-                            {MinusHPButton(-1)}
-                            {MinusHPButton(-5)}
-                        </ButtonGroup>
+                            Broken
+                        </Text>
+                    </>
+                );
+            }
+        };
+        const ShieldRaisedIcon = (props: any) => (
+            <Icon
+                {...props}
+                style={[props.style, { width: 32, height: 32 }]}
+                name="shield"
+            />
+        );
+        const ShieldDownIcon = (props: any) => (
+            <Icon
+                {...props}
+                style={[props.style, { width: 32, height: 32 }]}
+                name="shield-off"
+            />
+        );
+        const ShieldIcon = shield.isRaised ? ShieldRaisedIcon : ShieldDownIcon;
+        const handleShieldPress = () => {
+            props.raiseShield(shield.acBonus, shield.isRaised);
+            props.updateShield({ ...shield, isRaised: !shield.isRaised });
+        };
+        shields.push(
+            <Layout key={shield.id.toString()}>
+                <Text category="h5" style={{ textAlign: "center" }}>
+                    Shield
+                </Text>
+                <Layout style={{ flexDirection: "row" }}>
+                    {
+                        // TODO: Finish Raise shield Logic}
+                    }
+                    {shield.currentHP > shield.breakThreshold ? (
+                        <Button
+                            accessoryRight={ShieldIcon}
+                            appearance="ghost"
+                            onPress={handleShieldPress}
+                        />
+                    ) : (
+                        <></>
+                    )}
+                    <Layout style={styles.shield}>
+                        <Layout style={styles.horizontal}>
+                            <ButtonGroup
+                                style={styles.buttonGroup}
+                                size="tiny"
+                                status="danger"
+                            >
+                                {MinusHPButton(-1)}
+                                {MinusHPButton(-5)}
+                            </ButtonGroup>
+                            <TouchableOpacity
+                                onLongPress={editShield}
+                                style={styles.hpNumber}
+                            >
+                                <Text category="h6">
+                                    HP: {shield.currentHP!}/{shield.maxHP!}
+                                </Text>
+                            </TouchableOpacity>
+                            <ButtonGroup
+                                style={styles.buttonGroup}
+                                size="tiny"
+                                status="info"
+                            >
+                                {PlusHPButton(5)}
+                                {PlusHPButton(1)}
+                            </ButtonGroup>
+                        </Layout>
                         <TouchableOpacity
                             onLongPress={editShield}
-                            style={styles.hpNumber}
+                            style={styles.container}
                         >
-                            <Text category="h6">
-                                HP: {props.shield.currentHP!}/
-                                {props.shield.maxHP!}
-                            </Text>
+                            <Layout style={styles.horizontal}>
+                                {shieldData()}
+                            </Layout>
                         </TouchableOpacity>
-                        <ButtonGroup
-                            style={styles.buttonGroup}
-                            size="tiny"
-                            status="info"
-                        >
-                            {PlusHPButton(5)}
-                            {PlusHPButton(1)}
-                        </ButtonGroup>
                     </Layout>
-                    <TouchableOpacity
-                        onLongPress={editShield}
-                        style={styles.container}
-                    >
-                        <Layout style={styles.horizontal}>
-                            {shieldData()}
-                        </Layout>
-                    </TouchableOpacity>
                 </Layout>
             </Layout>
-        </Layout>
-    );
-
-    return shieldView;
+        );
+    });
+    return <Layout style={styles.container}>{shields}</Layout>;
 };
 
 type Props = LinkDispatchProps & LinkStateProps;
@@ -206,7 +204,7 @@ interface LinkDispatchProps {
 }
 
 interface LinkStateProps {
-    shield: Shield | undefined;
+    wornShields: Shield[] | undefined;
 }
 
 const mapDispatchToProps = (
@@ -222,11 +220,11 @@ const mapStateToProps = (state: EntireAppState): LinkStateProps => {
     const shields: Shield[] = state.playerCharacter.inventory.items.filter<Shield>(
         IsShield
     );
-    const wornShield: Shield | undefined = shields.find(
+    const wornShield: Shield[] | undefined = shields.filter(
         (shield) => shield.worn
     );
     return {
-        shield: wornShield,
+        wornShields: wornShield,
     };
 };
 
